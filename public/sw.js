479164b80e81b74f6a7f3d3d005173c7ea1e4001
Jason Yoo <jasonyoo0515@gmail.com>
2026-04-30 15:17:06 +0900

    fix: evict stale caches on service worker activation

--- public/sw.js
+++ public/sw.js
@@ -15,7 +15,14 @@ self.addEventListener("install", (event) => {
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))),
+      )
+      .then(() => self.clients.claim()),
+  );
 });
 
 self.addEventListener("fetch", (event) => {
